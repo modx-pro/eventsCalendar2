bf5eb54b4d49c6159e50bb477ab34cede293915c
bezumkin <bezumkin@yandex.ru>
2013-06-05 21:58:49 +0700

    Version 2.2.4

--- assets/components/eventscalendar2/js/eventscalendar2.js
+++ assets/components/eventscalendar2/js/eventscalendar2.js
@@ -1,33 +1,25 @@
-$(document).ready(function() {
+// Month changing
+$(document).on('click', '#Calendar .prev a, #Calendar .next a', function() {
+	var cal = $(this).parentsUntil('.calendar').parent();
+	var href = $(this).attr('href');
+	cal.css('opacity', 0.7);
 
-	// Смена месяца
-	$(document).on('click', '#Calendar .prev a, #Calendar .next a', function() {
-		var cal = $(this).parentsUntil('.calendar').parent();
-		var href = $(this).attr('href');
-		
-		$(cal).css('opacity', 0.7);
+	var width = cal.css('width');
+	var height = cal.css('height');
+	cal.parent().find('.cover').css({'width': width, 'height': height, 'margin-top': '-' + height}).show();
 
-		var width = $(cal).css('width');
-		var height = $(cal).css('height');
-		
-		$(cal).parent().find('.cover').css({'width': width, 'height': height, 'margin-top': '-' + height}).show();
-		
-		$.get(href, function(data) {
-				$(cal).parent().find('.cover').hide();
-				$('#Calendar .calendar').replaceWith(data);
-			}
-		)
-		return false;    
-	})
+	$.get(href, function(data) {
+			cal.parent().find('.cover').hide();
+			cal.replaceWith(data);
+		}
+	);
+	return false;
+});
 
-	// Показ событий
-	$('#Calendar .isevent').live('mouseenter',function() {
-		id = this.id;
-		$('#' + id + ' .event').show();
-	})
-	$('#Calendar .isevent').live('mouseleave',function() {
-		id = this.id;
-		$('#' + id + ' .event').hide();
-	})        
-
+// Display events
+$(document).on('mouseenter', '#Calendar .isevent', function() {
+	$('#' + this.id + ' .event').show();
+});
+$(document).on('mouseleave', '#Calendar .isevent', function() {
+	$('#' + this.id + ' .event').hide();
 });
